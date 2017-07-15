be9ec4fe186194f87af8d0f432f00dc09e1275f7
Ray Lui <rlui@unraveldata.com>
2017-07-15 22:56:45 +0000

    updated c7HDP_system_setup_v4.sh

--- HDP2.5/c7HDP_system_setup_v4.sh
+++ HDP2.5/c7HDP_system_setup_v4.sh
@@ -51,7 +51,7 @@ export JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:bin/java::")
 
 curl https://s3.amazonaws.com/aws-cli/awscli-bundle.zip -o awscli-bundle.zip
 unzip awscli-bundle.zip
-./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
 
 
 ##########################################################################################
