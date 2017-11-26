145a1462b4ec1395059e634065d615b0db94f90c
Ray Lui <rlui@unraveldata.com>
2017-11-26 15:43:22 -0800

    added new script

---
+++ 1126/unravel-setup.sh
@@ -0,0 +1,6 @@
+!#/bin/bash
+
+/usr/bin/wget http://preview.unraveldata.com/img/unravel-4.2-1061.x86_64.EMR.rpm /tmp/unravel-4.2-1061.x86_64.EMR.rpm
+sudo /usr/bin/rpm  -ivh --test /tmp/unravel-4.2-1061.x86_64.EMR.rpm
+
+
