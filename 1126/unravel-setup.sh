4da3fadbc42ef7aee8397133c86b550fc3d0d638
Ray Lui <rlui@unraveldata.com>
2017-11-26 22:17:08 -0800

    updated the script

--- 1126/unravel-setup.sh
+++ 1126/unravel-setup.sh
@@ -1,6 +1,7 @@
 !#/bin/bash
 
-/usr/bin/wget http://preview.unraveldata.com/img/unravel-4.2-1061.x86_64.EMR.rpm /tmp/unravel-4.2-1061.x86_64.EMR.rpm
-sudo /usr/bin/rpm  -ivh --test /tmp/unravel-4.2-1061.x86_64.EMR.rpm
-
-
+/usr/bin/wget http://preview.unraveldata.com/img/unravel-4.2-1061.x86_64.EMR.rpm
+/usr/bin/yum install -y ntp
+/usr/bin/systemctl enable ntpd
+/usr/bin/systemctl start ntpd
+/usr/bin/rpm  -U unravel-4.2-1061.x86_64.EMR.rpm
