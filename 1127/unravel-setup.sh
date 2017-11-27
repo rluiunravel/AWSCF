fb258fa6ff730fe5a716574f168bc2190ddd5deb
Ray Lui <rlui@unraveldata.com>
2017-11-27 10:02:07 -0800

    added new script

---
+++ 1127/unravel-setup.sh
@@ -0,0 +1,21 @@
+!#/bin/bash
+# Download unravel rpm
+/usr/bin/wget http://preview.unraveldata.com/img/unravel-4.2-1061.x86_64.EMR.rpm
+
+# Prepare the VM for unravel rpm install
+/usr/bin/yum install -y ntp
+/usr/bin/yum install -y libaio
+/usr/bin/yum install -y lzop
+/usr/bin/systemctl enable ntpd
+/usr/bin/systemctl start ntpd
+/usr/bin/setenforce 0
+
+# Prepare disk for unravel
+/usr/bin/parted -s /dev/sdb mklabel gpt mkpart primary 0% 100%
+/usr/sbin/mkfs.ext4 /dev/sdb1
+mkdir -p /srv
+echo "/dev/sdb1  /srv  ext4 defaults 0 0" >> /etc/fstab
+/usr/bin/mount -a
+
+# install unravel rpm
+/usr/bin/rpm  -U unravel-4.2-1061.x86_64.EMR.rpm
