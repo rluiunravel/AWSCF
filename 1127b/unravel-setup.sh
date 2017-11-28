2d040e0e498344e4f9cee748325a7ea9c111e13f
Ray Lui <rlui@unraveldata.com>
2017-11-28 11:34:37 -0800

    updated scripts

--- 1127b/unravel-setup.sh
+++ 1127b/unravel-setup.sh
@@ -10,15 +10,42 @@
 /usr/bin/setenforce 0
 
 # Prepare disk for unravel
+do_partition() {
+# This function creates one (1) primary partition on the
+# disk, using all available space
+    DISK=${1}
+    echo "n
+p
+1
+
+
+w"| fdisk "${DISK}" > /dev/null 2>&1
+
+#
+# Use the bash-specific $PIPESTATUS to ensure we get the correct exit code
+# from fdisk and not from echo
+if [ ${PIPESTATUS[1]} -ne 0 ];
+then
+    echo "An error occurred partitioning ${DISK}" >&2
+    echo "I cannot continue" >&2
+    exit 2
+fi
+}
+
+
 DATADISK=`/usr/bin/lsblk |grep 100G | awk '{print $1}'`
 echo $DATADISK > /tmp/datadisk
 
-/usr/sbin/parted -s /dev/${DATADISK} mklabel gpt mkpart primary 0% 100%
+#/usr/sbin/parted -s /dev/${DATADISK} mklabel gpt mkpart primary 0% 100%
+
+echo "Partitioning Disk ${DATADISK}"
+do_partition $DATADISK
+
 
 DISKP=`/usr/bin/cat /tmp/datadisk`
 DISKPARD=${DISKP}1
 
-/usr/sbin/mkfs -t ext4  /dev/${DISKPARD}
+#/usr/sbin/mkfs -t ext4  /dev/${DISKPARD}
 mkdir -p /srv
 echo "/dev/${DISKPARD}  /srv  ext4 defaults 0 0" >> /etc/fstab
 /usr/bin/mount -a
