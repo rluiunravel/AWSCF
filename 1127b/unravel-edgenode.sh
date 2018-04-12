d47cb002161d6ed06a97edf74832b7fbeb20d679
Ray Lui <rlui@unraveldata.com>
2018-04-12 10:35:33 -0700

    updated the unravel-edgenode.sh

--- 1127b/unravel-edgenode.sh
+++ 1127b/unravel-edgenode.sh
@@ -29,6 +29,7 @@ chown hdfs:hdfs /srv/unravel/log_hdfs
 chown hdfs:hdfs /srv/unravel/tmp_hdfs
 
 chown unravel:unravel /srv/unravel/k_data
+chown unravel:unravel /srv/unravel/tmp
 chown unravel:unravel /srv/unravel/s_1_data
 chown unravel:unravel /srv/unravel/zk_1_data
 chown unravel:unravel /srv/unravel/zk_2_data
@@ -42,18 +43,34 @@ sudo -u unravel sh -c 'echo "1" > /srv/unravel/zk_1_data/myid'
 sudo -u unravel sh -c 'echo "2" > /srv/unravel/zk_2_data/myid'
 sudo -u unravel sh -c 'echo "3" > /srv/unravel/zk_3_data/myid'
 
+# Create MySQL root password for unravel install
+cat /dev/urandom | tr -cd 'a-zA-Z0-9' | head -c10 > /usr/local/unravel/mysqlrootpass
+MYSQLROOTPASS=`cat /usr/local/unravel/mysqlrootpass`
+echo "MySQL root password = $MYSQLROOTPASS"
+
+cat /dev/urandom | tr -cd 'a-zA-Z0-9' | head -c10 > /usr/local/unravel/mysqlunravelpass
+MYSQLUNRAVELPASS=`cat /usr/local/unravel/mysqlunravelpass`
+echo "MySQL unravel password = $MYSQLUNRAVELPASS"
+
+sed -i -e "s/UMYSQLP/$MYSQLUNRAVELPASS/g" /usr/local/unravel/etc/unravel.properties
+
 ## Install mysql
 dpkg --configure -a
-echo "mysql-server mysql-server/root_password password UnravelMySQL123" | debconf-set-selections
-echo "mysql-server mysql-server/root_password_again password UnravelMySQL123" | debconf-set-selections
+echo "mysql-server mysql-server/root_password password $MYSQLROOTPASS" | debconf-set-selections
+echo "mysql-server mysql-server/root_password_again password $MYSQLROOTPASS" | debconf-set-selections
 apt-get install --assume-yes mysql-server
 
 service mysql start
 
-echo "create database unravel_mysql_prod DEFAULT CHARACTER SET utf8; grant all on unravel_mysql_prod.* TO 'unravel'@'%' IDENTIFIED BY 'CDWOM0hO'; use unravel_mysql_prod; source /usr/local/unravel/mysql_scripts/20170920015500.sql; source /usr/local/unravel/mysql_scripts/20171008153000.sql; source /usr/local/unravel/mysql_scripts/20171202224307.sql; source /usr/local/unravel/mysql_scripts/20180118103500.sql;" | mysql -u root -pUnravelMySQL123
+echo "create database unravel_mysql_prod DEFAULT CHARACTER SET utf8; grant all on unravel_mysql_prod.* TO 'unravel'@'%' IDENTIFIED BY '$MYSQLUNRAVELPASS'; use unravel_mysql_prod; source /usr/local/unravel/mysql_scripts/20170920015500.sql; source /usr/local/unravel/mysql_scripts/20171008153000.sql; source /usr/local/unravel/mysql_scripts/20171202224307.sql; source /usr/local/unravel/mysql_scripts/20180118103500.sql;" | mysql -u root -p$MYSQLROOTPASS
+
+echo "use unravel_mysql_prod; INSERT  IGNORE INTO \`users\` (\`id\`, \`email\`, \`encrypted_password\`, \`reset_password_token\`, \`reset_password_sent_at\`, \`remember_created_at\`, \`sign_in_count\`, \`current_sign_in_at\`, \`last_sign_in_at\`, \`current_sign_in_ip\`, \`last_sign_in_ip\`, \`created_at\`, \`updated_at\`, \`login\`, \`uid\`, \`authentication_token\`) VALUES (1,'','\$2a\$10\$.8bk4e/5UgD.A5ok13lKvOiVdzh.IMRbwrN0pJbvFZvXZHTitl5Di',NULL,NULL,NULL,1,now(),now(),'127.0.0.1','127.0.0.1',now(),now(),'admin',NULL,NULL); COMMIT;" | mysql -u root -p$MYSQLROOTPASS
 
 ## change permission on unravel daemon scripts
 chmod -R 755 /usr/local/unravel/init_scripts
+
+## Starting unravel daemons
+# /usr/local/unravel/init_scripts/unravel_all.sh start
 
 ## Completed the phase1 setup
 echo "All phase 1 processes are completed"
